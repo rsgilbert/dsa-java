d158daf97a663945152030b45bcbe72acad9599d
ssenyonjo <gssenyonjo@gmail.com>
2021-03-14 22:51:43 +0300

    implemented AbstractPriorityQueue

---
+++ src/priorityqueue/AbstractPriorityQueue.java
@@ -0,0 +1,63 @@
+package priorityqueue;
+
+import java.util.Comparator;
+
+/**
+ * An abstract base class to assist implementations of the PriorityQueue interface
+ */
+public abstract class AbstractPriorityQueue<K, V> implements PriorityQueue<K, V>{
+    // ----------- nested PQEntry class --------------
+    protected static class PQEntry<K,V> implements Entry<K, V> {
+        // key
+        private K k;
+        // value
+        private V v;
+        public PQEntry(K key, V value) {
+            k = key;
+            v = value;
+        }
+
+        public K getKey() { return k; }
+        public V getValue() { return v; }
+
+        // utilities not exposed as part of the Entry interface
+        protected void setKey(K key) { k = key; }
+        protected void setValue(V value) { v = value; }
+    }
+    // ---------- end of nested PQEntry class ----------
+
+    // instance variable for an AbstractPriorityQueue
+    /**
+     * The comparator defining the ordering of keys in the priority queue
+     */
+    private Comparator<K> comp;
+    /**
+     * Creates an empty priority queue using the given comparator to order keys
+     */
+    protected AbstractPriorityQueue(Comparator<K> c) { comp = c; }
+    /**
+     * Creates an empty priority queue based on the natural ordering of its keys
+     */
+    protected AbstractPriorityQueue() { this(new DefaultComparator<>()); }
+    /**
+     * Method for comparing two entries according to a key
+     */
+    protected int compare(Entry<K,V> a, Entry<K, V> b) {
+        return comp.compare(a.getKey(), b.getKey());
+    }
+    /**
+     * Determines whether a key is valid
+     */
+    protected boolean checkKey(K key) throws IllegalArgumentException {
+        try {
+            // See if key can be compared to itself
+            return (comp.compare(key, key) == 0);
+        } catch(ClassCastException e) {
+            throw new IllegalArgumentException("Incompatible key");
+        }
+    }
+    /**
+     * Tests whether the priority queue is empty
+     */
+    public boolean isEmpty() { return size() == 0; }
+}
