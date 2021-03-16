10c827ea615b0f9ccdaa3d97c819ae68e467b53e
ssenyonjo <gssenyonjo@gmail.com>
2021-03-16 08:19:16 +0300

    Implemented unsorted priority queue

--- src/priorityqueue/UnsortedPriorityQueue.java
+++ src/priorityqueue/UnsortedPriorityQueue.java
@@ -11,7 +11,7 @@ import java.util.Comparator;
  * @param <K> key
  * @param <V> value
  */
-public class UnsortedPriorityQueue<K,V> {//extends AbstractPriorityQueue<K,V> {
+public class UnsortedPriorityQueue<K,V> extends AbstractPriorityQueue<K,V> {
     /**
      * primary collection of priority queue entries
      */
@@ -26,16 +26,60 @@ public class UnsortedPriorityQueue<K,V> {//extends AbstractPriorityQueue<K,V> {
      * Creates an empty priority queue using the given comparator to order keys
      * @param comp comparator that will be used to order keys
      */
-    //public UnsortedPriorityQueue(Comparator<K> comp) { super(comp); }
+    public UnsortedPriorityQueue(Comparator<K> comp) { super(comp); }
 
     /**
      * Returns the position of an entry having minimum key
+     * Only called when the list is not empty
      * @return Position of entry with minimum key
      */
-//    private Position<Entry<K,V>> findMin() {
-//       Position<Entry<K,V>> small = list.first();
-//       for(Position<Entry<K,V> walk: list.positions()) {
-//
-//       }
-//    }
+    private Position<Entry<K,V>> findMin() {
+       Position<Entry<K,V>> small = list.first();
+       for(Position<Entry<K,V>> walk: list.positions()) {
+           // check if walk has a smaller key than that of the current small
+           if(compare(walk.getElement(), small.getElement()) < 0) {
+               small = walk;
+           }
+       }
+       return small;
+    }
+
+    /**
+     * Insert a key value pair and return the entry created
+     * @param key
+     * @param value
+     * @return
+     * @throws IllegalArgumentException
+     */
+    public Entry<K, V> insert(K key, V value) throws IllegalArgumentException {
+        // Auxiliary key-checking method (could throw an exception)
+        checkKey(key);
+        Entry<K, V> entry = new PQEntry<>(key, value);
+        list.addLast(entry);
+        return entry;
+    }
+
+    /**
+     * Returns without removing an entry with minimum key
+     * @return entry with minimum key
+     */
+    public Entry<K,V> min() {
+        if(list.isEmpty()) return null;
+        return findMin().getElement();
+    }
+
+    /**
+     * Removes and returns an entry with minimum key
+     * @return
+     */
+    public Entry<K, V> removeMin() {
+        if(list.isEmpty()) return null;
+        return list.remove(findMin());
+    }
+
+    /**
+     * Returns the number of items in the priority queue
+     * @return number of items in the priority queue
+     */
+    public int size() { return list.size(); }
 }
