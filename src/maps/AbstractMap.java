8d518360a022bd429b4ee974635c14dc96977f10
Gilbert <ssenyonjogilbert@gmail.com>
2021-04-18 00:22:30 +0300

    Created AbstractMap and wrote code for WordCount which is relying on the ChainHashMap that has not yet been implemented

---
+++ src/maps/AbstractMap.java
@@ -0,0 +1,73 @@
+package maps;
+
+
+import java.util.Iterator;
+
+/**
+ * Base class that provides the functionality that is shared by all of our map implementations
+ * @param <K> Key
+ * @param <V> Value
+ */
+public abstract class AbstractMap<K, V> implements Map<K,V> {
+    public boolean isEmpty() { return size() == 0; }
+
+    // -- nested MapEntry class --
+    protected static class MapEntry<K, V> implements Entry<K, V> {
+        private K k;
+        private V v;
+
+        public MapEntry(K key, V value) {
+            k = key;
+            v = value;
+        }
+
+        // public methods of the Entry interface
+        public K getKey() { return k; }
+        public V getValue() { return v; }
+
+        // utilities not exposed as part of the Entry interface
+        protected void setKey(K key) { k = key; }
+        protected V setValue(V value) {
+            V old = v;
+            v = value;
+            return old;
+        }
+    }
+    // -- End of MapEntry --
+
+
+    // -- Support for keySet() method --
+    private class KeyIterator implements Iterator<K> {
+        // reuse entrySet
+        final private Iterator<Entry<K, V>> entries = entrySet().iterator();
+        @Override
+        public boolean hasNext() { return entries.hasNext(); }
+        @Override
+        public K next() { return entries.next().getKey(); }
+    }
+    // Makes use of KeyIterator
+    private class KeyIterable implements Iterable<K> {
+        public Iterator<K> iterator() { return new KeyIterator(); }
+    }
+    public Iterable<K> keySet() { return new KeyIterable(); }
+    // -- keySet support end --
+
+    // -- Support for values() method --
+    private class ValueIterator implements Iterator<V> {
+        // reuse entrySet
+        final private Iterator<Entry<K,V>> entries = entrySet().iterator();
+        @Override
+        public boolean hasNext() { return entries.hasNext(); }
+        @Override
+        public V next() { return entries.next().getValue(); }
+    }
+    private class ValueIterable implements Iterable<V> {
+        @Override
+        public Iterator<V> iterator() { return new ValueIterator(); }
+    }
+    @Override
+    public Iterable<V> values() { return new ValueIterable(); }
+
+
+
+}
---
+++ src/maps/WordCount.java
@@ -0,0 +1,34 @@
+package maps;
+
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * A program that counts words in a document, printing the most frequent
+ */
+public class WordCount {
+    public static void main(String[] args) {
+        Map<String, Integer> freq = new ChainHashMap<>();
+
+        // Scan input for words, using all non-letters as delimiters
+        Scanner doc = new Scanner(System.in).useDelimiter("[^a-zA-Z]+");
+        while (doc.hasNext()) {
+            String word = doc.next().toLowerCase();
+            Integer count = freq.get(word);
+            if (count == null)
+                count = 0;
+            freq.put(word, count + 1);
+        }
+        int maxCount = 0;
+        String maxWord = "no word";
+        for (Entry<String, Integer> ent : freq.entrySet())
+            if (ent.getValue() > maxCount) {
+                maxCount = ent.getValue();
+                maxWord = ent.getKey();
+            }
+
+        System.out.println("The most frequent word is " + maxWord);
+        System.out.println("Max count is " + maxCount);
+    }
+}
