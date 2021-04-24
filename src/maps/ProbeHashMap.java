e668f19981024b073a0b8fd94c4bee5f6caa8ab6
Gilbert <ssenyonjogilbert@gmail.com>
2021-04-24 09:44:49 +0300

    First implementation of ProbeHashMap

--- src/maps/ProbeHashMap.java
+++ src/maps/ProbeHashMap.java
@@ -1,6 +1,10 @@
 package maps;
 
 
+import jdk.nashorn.internal.runtime.arrays.ArrayData;
+
+import java.util.ArrayList;
+
 /**
  * An implementation of ProbeHashMap using open addressing with linear probing
  * Meant for devices with small memory, hence optimized for space
@@ -61,13 +65,77 @@ public class ProbeHashMap<K,V> extends AbstractHashMap<K,V> {
         // Search has failed so we return available index for insertion wrapped according to our logic
         return -(avail + 1);
     }
+
+    /**
+     * Calculate corresponding index based on our convention for representing idx belonging to defunct slots or new slots in findSlot method
+     * @param idx index
+     * @return corresponding table index
+     */
+    private int calculateIdx(int idx) {
+        return -(idx + 1);
+    }
     // -- end of private utilities --
 
     /**
      * Returns value associated with key k in bucket with hash value h or null
+     * @param h hash value
+     * @param k key
+     * @return value, null if not found
      */
     protected V bucketGet(int h, K k) {
-        // TODO
+        int idx = findSlot(h, k);
+        if(idx >= 0)
+            return table[idx].getValue();
+        return null;
+    }
+
+    /**
+     * Associate key k with value v in bucket with hash value h, returns old value
+     * @param h hash value
+     * @param k key
+     * @param v value
+     * @return old value, null if new entry is created
+     */
+    protected V bucketPut(int h, K k, V v) {
+        int idx = findSlot(h, k);
+        if (idx >= 0) {
+            return table[idx].setValue(v);
+        }
+        int newIdx = calculateIdx(idx);
+        table[newIdx] = new MapEntry<>(k, v);
+        n++;
+        return null;
+    }
+
+    /**
+     * Removes entry having key k from bucket with hash value h, returns value of removed entry
+     * @param h hash value
+     * @param k key
+     * @return value of removed entry
+     */
+    protected V bucketRemove(int h, K k) {
+        int idx = findSlot(h, k);
+        if(idx >= 0) {
+            V answer = table[idx].getValue();
+            table[idx] = DEFUNCT;
+            n--;
+            return answer;
+        }
+        return null;
+    }
+
+    /**
+     * Returns an iterable collection of all key value pairs
+     * Order does not matter
+     * @return iterable collection of entries
+     */
+    public Iterable<Entry<K,V>> entrySet() {
+        ArrayList<Entry<K,V>> buffer = new ArrayList<>();
+        for(int idx = 0; idx < capacity; idx++) {
+            if(!isAvailable(idx))
+                buffer.add(table[idx]);
+        }
+        return buffer;
     }
 
 
